bcc2c30ac3cef322b8820ab57765ca6b567450b9
Cristina Silva <crissilva.eng@gmail.com>
2021-06-10 23:44:37 +0000

    feat: stockfish vs komodo

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -3,12 +3,15 @@ FROM python:3.8-buster
 WORKDIR /root
 
 ADD https://github.com/official-stockfish/Stockfish/archive/sf_13.tar.gz /root
-
 RUN tar xvzf *.tar.gz && cd Stockfish-sf_13/src \
     && make net && make build ARCH=x86-64-modern
+
+ADD https://komodochess.com/pub/komodo-12.zip /root
+RUN unzip *.zip
 
 FROM gitpod/workspace-full
 
 WORKDIR /root
 
 COPY --from=0 /root/Stockfish-sf_13/src/stockfish /usr/bin/stockfish
+COPY --from=0 /root/komodo-12.*/Linux/komodo-*-linux /usr/bin/komodo
--- main.ts
+++ main.ts
@@ -1,37 +1,82 @@
 import { Chess, ChessInstance } from 'chess.js'
 import { Engine } from 'node-uci'
 
-const play = async (engine: Engine, chess: ChessInstance) => {
-  await engine.init()
-  await engine.setoption('MultiPV', '4')
-  await engine.isready()
-
-  while (!chess.game_over()) {
-    await engine.position(chess.fen())
-    const { bestmove } = await engine.go({ movetime: 15 })
+class GameLogger {
+  log(chess: ChessInstance) {
     console.log(chess.ascii())
-    console.log(chess.turn(), chess.fen())
-    chess.move(bestmove, { sloppy: true })
+    console.log(chess.fen())
   }
-  console.log({
-    in_checkmate: chess.in_checkmate(),
-    in_stalemate: chess.in_stalemate(),
-    in_draw: chess.in_draw(),
-    insufficient_material: chess.insufficient_material(),
-    in_threefold_repetition: chess.in_threefold_repetition()
-  })
-  return chess
 }
 
-const chess = new Chess()
-const engine = new Engine('stockfish')
+class ChessEngine {
+  constructor(private readonly engine: Engine) {}
 
-play(engine, chess)
-  .then((chess) =>   console.log({
-    in_checkmate: chess.in_checkmate(),
-    in_stalemate: chess.in_stalemate(),
-    in_draw: chess.in_draw(),
-    insufficient_material: chess.insufficient_material(),
-    in_threefold_repetition: chess.in_threefold_repetition()
-  }))
-  .finally(() => engine.quit())
+  static async create(path: string) {
+    const engine = new Engine(path)
+    await engine.init()
+    await engine.isready()
+    return new ChessEngine(engine)
+  }
+
+  async move(fen: string, movetime: number): Promise<string> {
+    await this.engine.position(fen)
+    const { bestmove } = await this.engine.go({ movetime })
+    return bestmove
+  }
+}
+
+interface Outcome {
+  inCheckmate: boolean
+  inStalemate: boolean
+  inDraw: boolean
+  insufficientMaterial: boolean
+  inThreefoldRepetition: boolean
+}
+
+class Game {
+  constructor(
+    private readonly chess: ChessInstance,
+    private readonly players: Array<ChessEngine>,
+    private readonly logger: GameLogger) {}
+
+  async run(): Promise<Outcome> {
+    this.logger.log(this.chess)
+    while(!this.chess.game_over()) {
+      const player = this.player()
+      const move = await player.move(this.chess.fen(), 15)
+      this.chess.move(move, { sloppy: true })
+      this.logger.log(this.chess)
+    }
+    return this.outcome()
+  }
+
+  private player(): ChessEngine {
+    const player = this.players.shift()
+    this.players.push(player)
+    return player
+  }
+
+  private outcome(): Outcome {
+    return {
+      inCheckmate: this.chess.in_checkmate(),
+      inStalemate: this.chess.in_stalemate(),
+      inDraw: this.chess.in_draw(),
+      insufficientMaterial: this.chess.insufficient_material(),
+      inThreefoldRepetition: this.chess.in_threefold_repetition()
+    }
+  }
+}
+
+const main = async (white: string, black: string) => {
+  const chess = Chess()
+  const stockfish = await ChessEngine.create(white)
+  const komodo = await ChessEngine.create(black)
+  const logger = new GameLogger()
+  
+  const game = new Game(chess, [stockfish, komodo], logger)
+  const outcome = await game.run()
+  console.log(outcome)
+  process.exit()
+}
+
+main('stockfish', 'komodo')
